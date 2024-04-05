e679f4fc035c4fd5abb375f68168ff34bddd0c1c
Louis Chen <louischen2021uchi@gmail.com>
2024-04-05 21:30:48 +0800

    finish github oauth step 1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,35 @@
 'use client'
 
 import { title } from "@/components/primitives";
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
+import React, { useState } from 'react';
 
 export default function LoginPage() {
+	const [clientId, setClientId] = useState('');
+	const [secret, setSecret] = useState('');
+  
 	const handleLogin = () => {
-		const client_id = '';
-		const redirect_uri = '';
-		const scope = 'read:user user:email';
-		window.location.href = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`;
-	  };
+	  const redirect_uri = 'http://localhost:3000/login';
+	  const scope = 'read:user user:email';
+	  window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirect_uri}&scope=${scope}`;
+	};
+  
 	return (
-		<div>
-			<h1 className={title()}>Login</h1>
-			<Button onClick={handleLogin}>Login</Button>
+	  <div>
+		<div className="flex flex-col items-center gap-4">
+		  <h1 className={title()}>Login</h1>
+		  <Input 
+			label="Client ID" 
+			value={clientId}
+			onChange={(e) => setClientId(e.target.value)}
+			className="w-[150%]" />
+		  <Input 
+			label="Secret" 
+			value={secret}
+			onChange={(e) => setSecret(e.target.value)}
+		  	className="w-[150%]" />
+		  <Button onClick={handleLogin}>Login</Button>
 		</div>
+	  </div>
 	);
-}
+  }
