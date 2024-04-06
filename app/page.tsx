c6c1ab7d7f780f76bd47892aa2cd0604e2579908
Louis Chen <louischen2021uchi@gmail.com>
2024-04-06 23:12:07 +0800

    use github issue render table

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { title } from "@/components/primitives";
 import React from "react";
-import {Button, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner, getKeyValue} from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner, getKeyValue} from "@nextui-org/react";
 import {useInfiniteScroll} from "@nextui-org/use-infinite-scroll";
 import {useAsyncList} from "@react-stately/data";
 
@@ -11,68 +11,48 @@ export interface GithubIssue {
 	title: string;
 	body: string | null;
 	comments_url: string;
+	updated_at: string;
 }
-
+const username = 'Shih-Yang-Young';
+const repoName = 'issue-blog';
+async function fetchGithubData(cursor?: string | null): Promise<{ items: GithubIssue[], cursor: string | null }> {
+	const res = await fetch(cursor || `https://api.github.com/repos/${username}/${repoName}/issues`);
+	const json = await res.json();
+	const githubIssues: GithubIssue[] = json.map((issue: any) => ({
+		id: issue.id,
+		title: issue.title,
+		body: issue.body || null,
+		comments_url: issue.comments_url,
+		updated_at: issue.updated_at,
+	}));
+	return {
+		items: githubIssues,
+		cursor: json.next,
+	};
+}
 export default function Home() {
 	const [isLoading, setIsLoading] = React.useState(true);
 	const [hasMore, setHasMore] = React.useState(false);
 	const [loadedCount, setLoadedCount] = React.useState(0);
 	const [selectionBehavior, setSelectionBehavior] = React.useState("toggle");
 
-	let list = useAsyncList({
-		async load({signal, cursor}) {
-			if (cursor) {
-				setIsLoading(false);
-			}
-			// If no cursor is available, then we're loading the first page.
-			// Otherwise, the cursor is the next URL to load, as returned from the previous page.
-			const res = await fetch(cursor || "https://swapi.py4e.com/api/people/?search=", {signal});
-			let json = await res.json();
-
-			setHasMore(json.next !== null);
-
-			return {
-				items: json.results,
-				cursor: json.next,
-			};
+	let list = useAsyncList<GithubIssue>({
+		async load({ signal, cursor }) {
+		  if (cursor) {
+			setIsLoading(false);
+		  }
+	  
+		  const { items, cursor: nextCursor } = await fetchGithubData(cursor);
+		  setHasMore(nextCursor !== null);
+		  return { items, cursor: nextCursor || undefined }; // Adjust cursor type here
 		},
-	});
+	  });
 
-	const [loaderRef, scrollerRef] = useInfiniteScroll({hasMore, onLoadMore: list.loadMore});
-	
-	const username = 'Shih-Yang-Young';
-	const repoName = 'issue-blog';
-
-	const fetchGithubData = async () => {
-		fetch(`https://api.github.com/repos/${username}/${repoName}/issues`)
-		.then(response => response.json())
-		.then((issues: any[]) => {
-			const githubIssues: GithubIssue[] = issues.map(issue => ({
-				id: issue.id,
-				title: issue.title,
-				body: issue.body || null,
-				comments_url: issue.comments_url,
-			}));
-			console.log(`Issues for ${repoName}:`, githubIssues);
-		})
-		.catch(error => {
-			console.error(`Error fetching issues for ${repoName}:`, error);
-		});
-	};
+	const [loaderRef, scrollerRef] = useInfiniteScroll({hasMore, onLoadMore: list.loadMore});	
 	return (
 		<div>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className={title({ color: "violet" })}>{username}&nbsp;</div>
-				<br />
-				<div className="inline-block max-w-lg text-center justify-center">
-					<Button
-						id="getUserData"
-						className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
-						onClick={fetchGithubData}
-					>
-						Get GitHub Data
-					</Button>
-				</div>
+				<span className={`${title({size:"sm", color: "violet" })} lg:text-2xl`} >{username}&nbsp;</span>
 			</section>
 			<Table
 				isHeaderSticky
@@ -86,29 +66,29 @@ export default function Home() {
 					) : null
 				}
 				classNames={{
-					base: "max-h-[300px] overflow-scroll",
+					base: "max-h-[300px]",
 					table: "min-h-[300px]",
 				}}
 				onRowAction={(key) => alert(`Opening item ${key}...`)}
 			>
-				<TableHeader>
-					<TableColumn key="name">Name</TableColumn>
-					<TableColumn key="height">Height</TableColumn>
-					<TableColumn key="mass">Mass</TableColumn>
-					<TableColumn key="birth_year">Birth year</TableColumn>
-					<TableColumn key="button"><Button>aaa</Button></TableColumn>
-				</TableHeader>
-				<TableBody
-					isLoading={isLoading}
-					items={list.items}
-					loadingContent={<Spinner color="white" />}
-				>
-					{(item) => (
-					<TableRow key={item.name}>
-						{(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-					</TableRow>
-					)}
-				</TableBody>
+			<TableHeader>
+				<TableColumn key="id">Id</TableColumn>
+				<TableColumn key="title">Title</TableColumn>
+				<TableColumn key="body">Body</TableColumn>
+				<TableColumn key="updated_at">Updated At</TableColumn>
+				<TableColumn key="comments">Comments</TableColumn>
+			</TableHeader>
+			<TableBody
+				isLoading={isLoading}
+				items={list.items}
+				loadingContent={<Spinner color="white" />}
+			>
+				{(item) => (
+				<TableRow key={item.name}>
+					{(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+				</TableRow>
+				)}
+			</TableBody>
 			</Table>
 		</div>
 	);
