ced2849b6f07084273fcc6c20d7adf42913be0c6
Louis Chen <louischen2021uchi@gmail.com>
2024-04-06 12:09:59 +0800

    get access token from GithubProvider and get issues

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+// imports
+import NextAuth from "next-auth"
+
+// importing providers
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        })
+    ],
+    // https://stackoverflow.com/questions/69068495/how-to-get-the-provider-access-token-in-next-auth
+    callbacks: {
+      async jwt({ token, account }) {      
+        if (account) {
+          token = Object.assign({}, token, { access_token: account.access_token });
+        };
+        return token;
+      },
+      async session({ session, token }) {
+        if (session) {
+          session = Object.assign({}, session, { access_token: token.access_token })
+        };
+        return session;
+      }
+    }
+})
+
+export { handler as GET, handler as POST }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,98 @@
 // mark as client component
 "use client";
 
-// importing necessary functions
-import { useSession, signIn, signOut } from "next-auth/react"
-import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
-  // extracting data from usesession as session
-  const { data: session } = useSession()
+  // extracting data from useSession as session
+  const { data: session, status } = useSession();
 
-  // checking if sessions exists
+  // function to fetch GitHub access token
+  const fetchAccessToken = () => {
+    if (status === "authenticated") {
+      const { accessToken } = session as any;
+      console.log("aaa:", session?.access_token);
+      console.log("GitHub Access Token:", accessToken);
+    }
+  };
+  const getGithubIssue = () => {
+    const accessToken = session?.access_token;
+    
+    if (!accessToken) {
+      console.error('No access token available.');
+      return;
+    }
+
+    fetchGithubData(accessToken); 
+  };
+  const fetchGithubData = async (accessToken: any) => {
+    try {
+      const response = await fetch('https://api.github.com/repos/Shih-Yang-Chen/try-next/issues', {
+        method: 'GET', 
+        headers: {
+          'Authorization': `token ${accessToken}`,
+          'Accept': 'application/vnd.github.v3+json' // use GitHub REST API v3
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch GitHub issues');
+      }
+
+      const data = await response.json();
+      console.log(data); 
+    } catch (error) {
+      console.error('Error fetching GitHub issues:', error);
+    }
+  };
+
+  const handleClick = () => {
+    getGithubIssue();
+  };
+  // useEffect to fetch access token on session status change
+  useEffect(() => {
+    fetchAccessToken(); // initial fetch on component load
+
+    // fetch access token on session status change
+    if (status === "authenticated") {
+      fetchAccessToken();
+    }
+  }, [session, status]);
+
+  // checking if session exists
   if (session) {
-    // rendering components for logged in users
+    // rendering components for logged-in users
     return (
       <div className="w-full h-screen flex flex-col justify-center items-center">
-        <div className="w-44 h-44 relative mb-4">
-        <Image
-          src={session.user?.image as string}
-          fill
-          alt=""
-          className="object-cover rounded-full"
-        />
-        </div>
-        <p className="text-2xl mb-2">Welcome <span className="font-bold">{session.user?.name}</span>. Signed In As</p>
+        <p className="text-2xl mb-2">
+          Welcome <span className="font-bold">{session.user?.name}</span>. Signed In As
+        </p>
+        <button
+        id="getUserData"
+        className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
+        onClick={handleClick}
+      >
+        Get GitHub Data
+      </button>
         <p className="font-bold mb-4">{session.user?.email}</p>
-        <button className="bg-red-600 py-2 px-6 rounded-md" onClick={() => signOut()}>Sign out</button>
+        <button className="bg-red-600 py-2 px-6 rounded-md" onClick={() => signOut()}>
+          Sign out
+        </button>
       </div>
-    )
+    );
   }
 
-  // rendering components for not logged in users
+  // rendering components for not logged-in users
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-        <p className="text-2xl mb-2">Not Signed In</p>
-        <button className="bg-blue-600 py-2 px-6 rounded-md mb-2" onClick={() => signIn('google')}>Sign in with google</button>
-        <button className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2" onClick={() => signIn('github')}>Sign in with github</button>
+      <p className="text-2xl mb-2">Not Signed In</p>
+      <button
+        className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
+        onClick={() => signIn("github")}
+      >
+        Sign in with GitHub
+      </button>
     </div>
-  )
-
+  );
 }
