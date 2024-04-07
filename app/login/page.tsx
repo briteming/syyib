906e9e97a09eec02c79cebcd76098548f803b889
Louis Chen <louischen2021uchi@gmail.com>
2024-04-08 04:39:18 +0800

    remove not use code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,42 +2,10 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Octokit } from "@octokit/core";
-import { Button } from "@nextui-org/react";
 
 export default function Login() {
   const { data: session } = useSession();
 
-  const getGithubIssue = () => {
-    const accessToken = (session as any)?.access_token;
-
-    if (!accessToken) {
-      console.error("No access token available.");
-      return;
-    }
-
-    fetchGithubData(accessToken);
-  };
-  const fetchGithubData = async (accessToken: any) => {
-    try {
-      const octokit = new Octokit({ auth: `${accessToken}` });
-      const response = await octokit.request(
-        "GET /repos/Shih-Yang-Chen/try-next/issues",
-        {
-          headers: {
-            "X-GitHub-Api-Version": "2022-11-28",
-          },
-        },
-      );
-      if (!response) {
-        throw new Error("Failed to fetch GitHub issues");
-      }
-
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching GitHub issues:", error);
-    }
-  };
   if (session) {
     return (
       <div className="w-full h-screen flex flex-col justify-center items-center">
@@ -45,13 +13,6 @@ export default function Login() {
           Welcome <span className="font-bold">{session.user?.name}</span>.
           Signed In As
         </p>
-        <Button
-          id="getUserData"
-          className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
-          onClick={getGithubIssue}
-        >
-          Get GitHub Data
-        </Button>
         <p className="font-bold mb-4">{session.user?.email}</p>
         <button
           className="bg-red-600 py-2 px-6 rounded-md"
